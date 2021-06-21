51301bbf20132c6f9c2b20a32e7f05f2bf8ab145
FeliAlbornoz <felipealbornoz4@gmail.com>
2021-06-21 16:53:58 -0400

    commit changes

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home, login
+from .views import home, login, contacto, detalle, signup, feed
 
 urlpatterns=[
         path('', home, name="home"),
-        path('login/', login, name="login")
+        path('login/', login, name="login"),
+        path('contacto/', contacto, name="contacto"),
+        path('detalle/', detalle, name="detalle"),
+        path('signup/', signup, name="signup"),
+        path('feed/', feed, name="feed")
 ]
--- core/views.py
+++ core/views.py
@@ -4,11 +4,28 @@ from django.shortcuts import render
 
 def home(request):
 
-    contexto={"nombre":"Jorge",
-            "edad":"24"}
-
-    return render(request, 'core/home.html', contexto)
+    return render(request, 'core/home.html')
 
 def login(request):
 
         return render(request, 'core/login.html')
+
+
+def signup(request): 
+
+        return render(request, 'core/signup.html')
+
+
+def contacto(request): 
+
+        return render(request, 'core/contacto.html')
+
+
+def detalle(request): 
+
+        return render(request, 'core/detalle.html')
+        
+
+def feed(request): 
+
+        return render(request, 'core/feed.html')
