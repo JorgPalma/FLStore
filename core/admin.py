d070d25be90d8c9cfb9e67054911c41972aaea2a
Jorge <jorg.palma@duocuc.cl>
2021-06-26 20:17:10 -0400

    commit bdd

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import categoria, post
 
 # Register your models here.
+
+admin.site.register(categoria)
+admin.site.register(post)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields import NullBooleanField
 
 # Create your models here.
+class categoria(models.Model):
+    nombreCat = models.CharField(verbose_name="Nombre categoria", max_length=20)
+
+    def __str__(self):
+        return self.nombreCat
+
+class post(models.Model):
+    nombreP = models.CharField(verbose_name="Nombre producto", max_length=25)
+    descripcionP = models.CharField(verbose_name="Descipcion producto", max_length=700)
+    miniDescripcion = models.CharField(verbose_name="Descripcion abreviada", max_length=30)
+    precioP = models.IntegerField(verbose_name="Precio producto")
+    nombreU = models.CharField(verbose_name="Nombre de usuario", max_length=20)
+    emailU = models.CharField(verbose_name="Correo de usuario", max_length=30)
+    telefonoU = models.CharField(verbose_name="Teléfono de usuario", max_length=12, blank=True)
+    instagramU = models.CharField(verbose_name="Instagram de usuario", max_length=20, blank=True)
+    facebookU = models.CharField(verbose_name="Facebook de usuario", max_length=25, blank=True)
+    twitterU = models.CharField(verbose_name="Twitter de usuario", max_length=25, blank=True)
+    whatsappU = models.CharField(verbose_name="Whatsapp de usuario", max_length=12, blank=True)
+    categoriaP = models.ForeignKey(categoria, on_delete=models.CASCADE)
+    imgP = models.ImageField(upload_to="productos", null=True, verbose_name="Imagen producto")
+
+    def __str__(self):
+        return f"Usuario: {self.nombreU}, Post: {self.nombreP}"
+
